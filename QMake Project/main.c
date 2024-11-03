af5e2dbf4610a5b138b962d1297d80f9204bbb35
at0mb0mb4 <42113144+cptihar@users.noreply.github.com>
2024-11-03 18:03:11 +0100

    Add files via upload

---
+++ QMake Project/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "test.h"
+
+int main(int argc, char** argv) {
+
+    test();
+
+    return 0;
+}
---
+++ QMake Project/test.h
@@ -0,0 +1,6 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <stdio.h>
+void test();
+#endif
